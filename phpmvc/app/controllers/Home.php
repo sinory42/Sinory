743b2f2c7adcba41c91df8dd7fdeff02ed6859f5
sinory42 <55267149+sinory42@users.noreply.github.com>
2019-10-07 17:09:09 +0700

    php mvc
    Membuat PHP dengan MVC

---
+++ phpmvc/app/controllers/Home.php
@@ -0,0 +1,12 @@
+<?php
+
+class Home extends Controller {
+    public function index()
+    {
+       // echo 'home/index';
+       $data['judul'] = 'Home';
+       $this->view('templates/header', $data);
+       $this->view('home/index');
+       $this->view('templates/footer');
+    }
+}
